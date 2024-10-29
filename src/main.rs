73e0fe0e6148b8f773bfa9f910440cc6749ab395
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2024-10-29 11:53:26 +0700

    Added a controller to accept a put-records request

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,9 @@ use serde::{Deserialize, Serialize};
 
 #[tokio::main]
 async fn main() {
-
-    // build our application with a route
     let app = Router::new()
-        // `GET /` goes to `root`
         .route("/", get(root))
-        // `POST /users` goes to `create_user`
+        .route("/add", post(put_records))
         .route("/users", post(create_user));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -24,28 +21,42 @@ async fn root() -> &'static str {
 }
 
 async fn create_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
     Json(payload): Json<CreateUser>,
 ) -> (StatusCode, Json<User>) {
-    // insert your application logic here
     let user = User {
         id: 1337,
         username: payload.username,
     };
-
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
+    println!("Hello, world!");
+    println!("{}", user.username);
     (StatusCode::CREATED, Json(user))
 }
 
-// the input to our `create_user` handler
+async fn put_records(
+    Json(payload): Json<PutRecord>,
+) -> StatusCode {
+    println!("{}", payload.stream_name);
+    println!("{}", payload.partition_key);
+    println!("{}", match payload.explicit_hash_key {
+        Some(x) => x,
+        None    => "nu".to_string(),
+    });
+    StatusCode::NO_CONTENT
+}
+
+#[derive(Deserialize)]
+struct PutRecord {
+    stream_name: String,
+    partition_key: String,
+    explicit_hash_key: Option<String>,
+    data: Vec<u8>,
+}
+
 #[derive(Deserialize)]
 struct CreateUser {
     username: String,
 }
 
-// the output to our `create_user` handler
 #[derive(Serialize)]
 struct User {
     id: u64,
